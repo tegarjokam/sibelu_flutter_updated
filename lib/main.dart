ef0594deb6ca44e0f7546bbbc70fff6dfec29488
tegarjokam <dtegarjokam354@gmail.com>
2021-03-11 14:36:26 +0900

    try to fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,37 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final TextTheme textTheme = TextTheme(
+    headline1: GoogleFonts.poppins(
+        fontSize: 93, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+    headline2: GoogleFonts.poppins(
+        fontSize: 58, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+    headline3: GoogleFonts.poppins(fontSize: 46, fontWeight: FontWeight.w400),
+    headline4: GoogleFonts.poppins(
+        fontSize: 33, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+    headline5: GoogleFonts.poppins(fontSize: 23, fontWeight: FontWeight.w400),
+    headline6: GoogleFonts.poppins(
+        fontSize: 19, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+    subtitle1: GoogleFonts.poppins(
+        fontSize: 15, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+    subtitle2: GoogleFonts.poppins(
+        fontSize: 13, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+    bodyText1: GoogleFonts.karla(
+        fontSize: 18, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+    bodyText2: GoogleFonts.karla(
+        fontSize: 15, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+    button: GoogleFonts.karla(
+        fontSize: 15, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+    caption: GoogleFonts.karla(
+        fontSize: 13, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+    overline: GoogleFonts.karla(
+        fontSize: 11, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SiBeLu',
+      theme: ThemeData(),
       home: MyHomePage(title: 'SiBeLu App'),
     );
   }
